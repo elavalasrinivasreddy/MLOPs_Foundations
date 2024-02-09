4ce38967e2cebdc058b1f06b40796fa32cc92cb1
elavalasrinivasreddy <elavalasrinivasreddy@github.com>
2024-02-09 20:32:23 +0530

    project template/folders are created

---
+++ project_template.py
@@ -0,0 +1,44 @@
+import os
+from pathlib import Path
+# import logging
+
+# Below is the list of files we need in project
+list_files = [
+    ".github/workflows/.gitkeep",
+    "src/__init__.py",
+    "src/components/__init__.py",
+    "src/components/data_ingestion.py",
+    "src/components/data_transformation.py",
+    "src/components/model_training.py",
+    "src/components/model_evaluation.py",
+    "src/pipeline/__init__.py",
+    "src/pipeline/training_pipeline.py",
+    "src/pipeline/prediction_pipeline.py",
+    "src/utils/__init__.py"
+    "src/utils/utils.py",
+    "src/logger/logging.py",
+    "src/exceptions/exception"
+    "test/unit/__init__.py",
+    "test/integration/__init__.py",
+    "init_setup.sh",
+    "requirements.txt",
+    "requirements_dev.txt",
+    "setup.py",
+    "setup.cfg",
+    "pyproject.toml",
+    "tox.ini",
+    "experiment/experiments.ipynb"
+]
+
+for filepath in list_files:
+    # changing the path slashes
+    filepath = Path(filepath)
+    # extracting directory and file names
+    file_dir, file_name = os.path.split(filepath)
+    #if directory path exists creating a directory
+    if file_dir != "":
+        os.makedirs(file_dir, exist_ok=True)
+        # logging.info(f"Creating directory: {file_dir}")
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass # creating an empty file
